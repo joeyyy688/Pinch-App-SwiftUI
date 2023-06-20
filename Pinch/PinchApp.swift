df0e13f28825b61cc01d8ea076fc540edf82643d
joeyyy688 <edinjoey@gmail.com>
2023-06-20 01:09:52 +0000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Joseph on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
